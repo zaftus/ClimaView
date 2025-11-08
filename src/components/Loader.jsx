a14b5e728dc0103002694befa834801a3fbc7db0
Ankith <160985511+zaftus@users.noreply.github.com>
2025-11-07 19:37:39 -0500

    Create Loader.jsx

---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+export default function Loader() {
+  return (
+    <div className="mt-8">
+      <div className="w-12 h-12 border-4 border-indigo-500 border-t-transparent rounded-full animate-spin mx-auto"></div>
+      <p className="text-gray-600 mt-2">Loading weather data...</p>
+    </div>
+  );
+}
